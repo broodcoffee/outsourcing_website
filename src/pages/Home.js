00b674eaf7ae1c47dbfab8b9a302e32f85ea74d7
Rhodette Grace Saguid <rg.saguid@gmail.com>
2021-12-01 12:42:21 +0800

    removed duplicate paragraph on main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,11 +9,6 @@ const Home = () => {
     return (
         <>
             <div id='main'>
-                <div className='header-heading' id="heading">
-                    <h3>Hello, Welcome to Code Sourcing its nice to be you here!</h3><br />
-                    <h1><span>CODE</span> FOR<br /> WEEK</h1><br /><br />
-                        <a href='#section' className='header-btn'>Dev</a>
-                </div>
                 <div className='header-heading'>
                     <h3>Want a place to start your career?</h3><br />
                     <h1><span>LOOK</span> RIGHT <br /> HERE</h1><br /><br />
